2271413ea07a3283ca6d08cbbaf011de5895af39
purbayankar <53092711+purbayankar@users.noreply.github.com>
2021-02-20 14:57:10 +0530

    Update meta.py

--- meta.py
+++ meta.py
@@ -86,7 +86,7 @@ class Learner(nn.Module):
 		# pred: [setsz, n_way], indices: [setsz]
 		_, indices = torch.max(pred, dim=1)
 # 		correct = torch.eq(indices, query_y).sum().data[0]
-		correct = torch.eq(indices, query_y).sum().item()
+		correct = torch.eq(indices, query_y).sum().data.item()
 		acc = correct / query_y.size(0)
 
 		# gradient for validation on theta_pi
