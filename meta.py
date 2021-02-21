4c6138a6524c65c58ffb8eb84c29e56114b2d0bd
purbayankar <53092711+purbayankar@users.noreply.github.com>
2021-02-21 20:27:48 +0530

    Update meta.py

--- meta.py
+++ meta.py
@@ -219,7 +219,7 @@ class MetaLearner(nn.Module):
 				sum_grads_pi = grad_pi
 			else:  # accumulate all gradients from different episode learner
 # 				print("The cosine angle is {}".format(cosine_angle(self.store_grad, grad_pi)))
-				sum_grads_pi = [cosine_angle(self.store_grad, grad_pi)*torch.add(i, j) for i, j in zip(sum_grads_pi, grad_pi)]
+				sum_grads_pi = [(1-cosine_angle(self.store_grad, grad_pi))*torch.add(i, j) for i, j in zip(sum_grads_pi, grad_pi)]
 # 		print("The len :{}".format(len(sum_grads_pi)))
 		self.store_grad = sum_grads_pi
 			
